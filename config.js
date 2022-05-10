c68e5bfd594eda175db73863e9d658e3d7623bc0
ahmedElsehrawy <aelsehrawy1999@gmail.com>
2022-05-10 10:12:38 +0200

    change from nodemon to just node

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://elsehrawy:5eYXSAWeJJ92j4sv@cluster0.hlhmx.mongodb.net/socialmedia?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
