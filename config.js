643cb11aaa7727c6b30fe053d0c102dcc8f28171
ahmedElsehrawy <65005663+ahmedElsehrawy@users.noreply.github.com>
2023-11-04 18:24:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://elsehrawy:5eYXSAWeJJ92j4sv@cluster0.hlhmx.mongodb.net/socialmedia?retryWrites=true&w=majority",
+    "mongodb+srv://elsehrawy:8GH5uskDI4GNb6IP@cluster0.hlhmx.mongodb.net/socialmedia?retryWrites=true&w=majority",
   SECRET_KEY: "some very secret key",
 };
